5b16b467a4957c444fa83a03a7d0bf6928970e46
nyeohijyul <nyeohijyul@gmail.com>
2026-06-01 15:31:59 +0900

    feat: cold start 방지 - uptimerobot을 위한 HttpResponse 추가

--- backend/audiofit/urls.py
+++ backend/audiofit/urls.py
@@ -5,8 +5,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK", content_type="text/plain")
 
 urlpatterns = [
+    path('', health_check, name='health_check'),
+    
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include('apps.users.urls')),
     path('api/v1/', include('apps.clips.urls')),
